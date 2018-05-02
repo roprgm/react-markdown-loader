2b170d6be8cdc664fbd19c84ede015a340d4ec44
Rodrigo Belfiore <rodrigobelfiore@gmail.com>
2018-05-02 11:40:39 -0300

    Update parser.js

--- src/parser.js
+++ src/parser.js
@@ -83,6 +83,7 @@ function parseMarkdown(markdown) {
         const language = Prism.languages[lang] || Prism.languages.autoit;
         return Prism.highlight(code, language);
       },
+      html: true,
       xhtmlOut: true
     };
 
